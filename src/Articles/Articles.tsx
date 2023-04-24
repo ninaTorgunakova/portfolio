639a0c84f70bf9c19db67e5c750f8308acc59387
Nina Torgunakova <ninatorgunakova@gmail.com>
2023-04-24 12:35:27 +0400

    Add Nano Stores in Angular article

--- src/Articles/Articles.tsx
+++ src/Articles/Articles.tsx
@@ -3,6 +3,11 @@ import { ArticleInfo } from './Article';
 import Article from './Article';
 
 const ARTICLES: ArticleInfo[] = [{
+  title: 'Nano Stores in Angular: how to make the state management simpler',
+  description: 'The article is about how to use Nano Stores in Angular and what are its advantages.',
+  link: 'https://dev.to/evilmartians/nano-stores-in-angular-how-to-make-the-state-management-simpler-38a1',
+  iconPath: 'icons/nanostores.svg'
+}, {
   title: 'OKLCH в CSS: по­че­му мы ушли от RGB и HSL',
   description: 'Russian translation of the article about OKLCH in CSS.',
   link: 'https://web-standards.ru/articles/oklch-in-css-why-quit-rgb-hsl/',
